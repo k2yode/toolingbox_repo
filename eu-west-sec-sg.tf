4623793dd13eadd1d372c98abc6576cf55f9f2d3
k2yode <kay_ode@outlook.com>
2020-10-02 11:25:17 +0100

    updated

---
+++ eu-west-sec-sg.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "eu-west-sec-sg" {
+  name = "AWSTerraform-SecGroup"
+
+  tags = {
+    Name = "EU-West-Firewall"
+  }
+
+  #Inbound SSH from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  #Inbound http from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #Inbound http from anywhere
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #Outbound to anywhere
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ tooling.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "k2yode-eu-west-bucket"
+    key    = "euwest.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+resource "aws_instance" "instance_x" {
+  ami   = "ami-0f79f2f1eb693b53b"
+  count = 2
+
+  #instance type
+  instance_type = "t2.micro"
+
+  #instance tags
+  tags = {
+    Name = "eu-Tooling-Server-${count.index + 1}"
+  }
+
+  #create security group
+  vpc_security_group_ids = ["${aws_security_group.eu-west-sec-sg.id}"]
+
+  #passing key
+  key_name = "eu-devopskey"
+}
